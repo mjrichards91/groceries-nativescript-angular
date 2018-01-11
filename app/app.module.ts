c08c9d0c13df783260e9649e9b93647d175a44be
Mike Richards <mjrichards91@gmail.com>
2018-01-11 12:49:15 -0500

    Moved view from template to login and created a user model

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [NativeScriptModule],
+  imports: [
+    NativeScriptModule,
+    NativeScriptFormsModule
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
