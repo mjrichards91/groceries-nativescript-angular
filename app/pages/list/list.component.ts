9e56f523ffe6fe4c49e22ee19f66ace44a2f80a7
Mike Richards <mjrichards91@gmail.com>
2018-01-11 16:33:59 -0500

    Added the ability to share grocery list
    Added action bar and share button to use the social share package in the list component.

--- app/pages/list/list.component.ts
+++ app/pages/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { Grocery } from "../../shared/grocery/grocery";
 import { GroceryListService } from "../../shared/grocery/grocery-list.service";
 import { TextField } from "ui/text-field";
+import * as SocialShare from "nativescript-social-share";
 
 @Component({
     selector: "list",
@@ -56,5 +57,13 @@ export class ListComponent implements OnInit {
                 this.grocery = "";
             }
             )
+    }
+
+    share() {
+        let listString = this.groceryList
+            .map(grocery => grocery.name)
+            .join(", ")
+            .trim();
+        SocialShare.shareText(listString);
     }
 }
