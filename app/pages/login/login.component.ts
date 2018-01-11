8f32e6f55ddfc4b70c5b115bea3d9d34b5f0f86c
Mike Richards <mjrichards91@gmail.com>
2018-01-11 16:51:12 -0500

    Added plaform specific styling to login page
    Updates the hint text color on the login credential text fields.

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -5,6 +5,8 @@ import { Router } from "@angular/router";
 import { Page } from 'ui/page';
 import { Color } from 'color';
 import { View } from 'ui/core/view';
+import { setHintColor } from "../../utils/hint-util";
+import { TextField } from "ui/text-field";
 
 @Component({
     selector: "my-app",
@@ -16,6 +18,8 @@ export class LoginComponent implements OnInit {
     user: User;
     isLoggingIn = true;
     @ViewChild("container") container: ElementRef;
+    @ViewChild("email") email: ElementRef;
+    @ViewChild("password") password: ElementRef;
 
     constructor(
         private router: Router,
@@ -66,11 +70,26 @@ export class LoginComponent implements OnInit {
 
     toggleDisplay() {
         this.isLoggingIn = !this.isLoggingIn;
+        this.setTextFieldColors();
+
         let container = <View>this.container.nativeElement;
 
         container.animate({
             backgroundColor: this.isLoggingIn ? new Color('white') : new Color('#301217'),
             duration: 200
         });
+    }
+
+    setTextFieldColors() {
+        let emailTextField = <TextField>this.email.nativeElement;
+        let passwordTextField = <TextField>this.password.nativeElement;
+
+        let mainTextColor = new Color(this.isLoggingIn ? "black" : "#C4AFB4");
+        emailTextField.color = mainTextColor;
+        passwordTextField.color = mainTextColor;
+
+        let hintColor = new Color(this.isLoggingIn ? "#ACA6A7" : "#C4AFB4");
+        setHintColor({ view: emailTextField, color: hintColor });
+        setHintColor({ view: passwordTextField, color: hintColor });
     }
 }
