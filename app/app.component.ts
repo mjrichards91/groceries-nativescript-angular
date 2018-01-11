b5ea34e12f534b186ca45322dfc1803b7364aeac
Mike Richards <mjrichards91@gmail.com>
2018-01-11 13:30:29 -0500

    Added register functionality to user service
    Included Http module as well

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
 import { User } from './shared/user/user';
+import { UserService } from "./shared/user/user.service";
 
 @Component({
   selector: "my-app",
+  providers: [UserService],
   templateUrl: 'pages/login/login.html',
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
@@ -10,13 +12,32 @@ export class AppComponent {
   user: User;
   isLoggingIn = true;
 
-  constructor() {
+  constructor(private userService: UserService) {
     this.user = new User();
   }
 
   submit() {
-    alert("You’re using: " + this.user.email);
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
   }
+  login() {
+    // TODO: Define
+  }
+
+  signUp() {
+    this.userService.register(this.user)
+      .subscribe(
+      () => {
+        alert("Your account was successfully created.");
+        this.toggleDisplay();
+      },
+      () => alert("Unfortunately we were unable to create your account.")
+      );
+  }
+
   toggleDisplay() {
     this.isLoggingIn = !this.isLoggingIn;
   }
